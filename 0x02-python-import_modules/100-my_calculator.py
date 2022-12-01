50ac3a388853c4c256674760d75e77dfd272aa8a
‘Azubuikeleo13’ <azubuikeleo13@gmail.com>
2022-12-01 08:22:31 -0800

    A Basic Calculator

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -8,7 +8,7 @@ if __name__ == '__main__':
     a = int(sys.argv[1])
     b = int(sys.argv[3])
 
-    count = len(sys.argv) -1
+    count = len(sys.argv) - 1
 
     if count != 3:
         print('Usage: ./100-my_calculator.py <a> <operator> <b>')
