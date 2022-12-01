b8ed69fc5e8a6e94f3f1422b8b77b3bc5e03fe38
‘Azubuikeleo13’ <azubuikeleo13@gmail.com>
2022-12-01 07:31:55 -0800

    Summing the command line arguments

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -4,12 +4,9 @@ if __name__ == '__main__':
 
     import sys
 
-    no_arg = len(sys.argv) - 1
-    summ = 0
+    total = 0
 
-    if no_arg == 0:
-        print('{}.'.format(summ))
-    else:
-        for i in range(1, len(sys.argv)):
-            summ += int(sys.argv[i])
-        print(summ)
+    for i in range(len(sys.argv) - 1):
+        total += int(sys.argv[i + 1])
+
+    print("{}".format(total))
