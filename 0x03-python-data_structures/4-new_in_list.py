0d580fe03216d0d9320882c1a684b1c6275f8a19
‘Azubuikeleo13’ <azubuikeleo13@gmail.com>
2022-12-05 10:57:56 -0800

    Replacing an element of a copied list

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -2,12 +2,10 @@
 
 def new_in_list(my_list, idx, element):
 
-    my_list_cp = my_list.copy
+    copy = my_list.copy()
 
-    if idx < 0:
-        return my_list.copy
-    elif idx > len(my_list) - 1:
-        return my_list.copy
+    if idx < 0 or idx > len(my_list) - 1:
+        return my_list.copy()
     else:
-        my_list_cp[idx] = element
-        return my_list_cp
+        copy[idx] = element
+        return copy
