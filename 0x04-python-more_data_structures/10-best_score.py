cc336f75780f30e46d4a6da222f0367d97103172
‘Azubuikeleo13’ <azubuikeleo13@gmail.com>
2022-12-07 13:21:45 -0800

    Getting the key with hightest number

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+def best_score(a_dictionary):
+
+    if a_dictionary == None:
+        return None
+
+    for i in a_dictionary.keys():
+        if a_dictionary.get(i) > bst:
+            nam = i
+
+    return nam
