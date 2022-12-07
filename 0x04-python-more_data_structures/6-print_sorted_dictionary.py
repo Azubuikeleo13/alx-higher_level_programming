62c079a6842ad508174a8e67dc0ca1e4003d2117
‘Azubuikeleo13’ <azubuikeleo13@gmail.com>
2022-12-07 11:31:11 -0800

    Printing a sorted Dictionary using Keys

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -5,6 +5,5 @@ def print_sorted_dictionary(a_dictionary):
     kys = sorted(a_dictionary)
     kys.sort()
 
-
     for i in kys:
-        print('{:s}: {}'.format(i,a_dictionary.get(i)))
+        print('{:s}: {}'.format(i, a_dictionary.get(i)))
