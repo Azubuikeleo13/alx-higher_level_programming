a4654ca47477ba706ce4809882cf33e902217c6a
‘Azubuikeleo13’ <azubuikeleo13@gmail.com>
2022-12-07 13:58:54 -0800

    Getting the key with hightest number

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -2,14 +2,17 @@
 
 def best_score(a_dictionary):
 
-    if a_dictionary is None:
-        return
+    """Returns a key with the biggest integer value."""
 
-    bst = 0
+    if not isinstance(a_dictionary, dict) or len(a_dictionary) == 0:
+        return None
 
-    for i in a_dictionary.keys():
-        if a_dictionary.get(i) > bst:
-            bst = a_dictionary.get(i)
-            nam = i
+    ret = list(a_dictionary.keys())[0]
+    big = a_dictionary[ret]
 
-    return nam
+    for k, v in a_dictionary.items():
+        if v > big:
+            big = v
+            ret = k
+
+    return (ret)
