e08b978d9c33bb1eab4069439d676611df7da460
‘Azubuikeleo13’ <azubuikeleo13@gmail.com>
2022-12-07 15:00:26 -0800

    Getting the weighted average

---
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+
+def weight_average(my_list=[]):
+
+    if my_list is []:
+        return 0
+
+    new_ls = []
+
+    for i in my_list:
+        prd = 1
+
+        for j in i:
+           prd *= j
+
+        new_ls.append(prd)
+
+    summ = sum(new_ls)
+    wei = 0
+
+    for i in my_list:
+        for j in range(1, len(i)):
+            wei += i[j]
+
+    aver = summ / wei
+
+    return aver
